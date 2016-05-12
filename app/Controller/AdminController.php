84c2942f18db948e47c66cbd7a269d615400933e
chama <tomo00223@yahoo.co.jp>
2016-05-12 16:54:22 +0900

    個別決済画面発行ページのモックをFormヘルパー使用コードに修正,入力フォームのValidate部分作成

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -1,7 +1,22 @@
 <?php
 class AdminController extends AppController {
+
+
     public function generate() {
+        $this->layout = 'adminLayout';
+
+        if ($this->request->is('post'))
+        {
+            $this->Admin->set($this->request->data);
+
+            $this->Admin->validates();
+
+        }
+    }
+
+    public function generated() {
       $this->layout = 'adminLayout';
+
 
     }
 }
